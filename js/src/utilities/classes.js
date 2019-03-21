0191b9485c0ee94c74e1fe31dc88b33ae61d0969
Bruna Martini <abrunamartini@gmail.com>
2019-03-21 19:00:26 -0300

    2.1.20, changing popover css

--- js/src/utilities/classes.js
+++ js/src/utilities/classes.js
@@ -1,6 +1,6 @@
 /**
  * --------------------------------------------------------------------------
- * CoreUI Utilities (v2.1.19): classes.js
+ * CoreUI Utilities (v2.1.20): classes.js
  * Licensed under MIT (https://coreui.io/license)
  * --------------------------------------------------------------------------
  */
